4beff9d11ccb8c367c10fe54cdd5183d22840754
Michael McClure <77661117+mimcclure@users.noreply.github.com>
2023-08-16 16:44:12 -0400

    Add files via upload

---
+++ array_1.py
@@ -0,0 +1,23 @@
+import array
+
+arr = array.array('i',[1,2,3]) #signed integers for array values
+
+print ("New array is: ", end="") #end used for newlines
+for i in range (0, 3):
+    print (arr[i], end=" ")
+
+print("\r")
+
+arr.append(4) #append value to the end of the array
+
+print("Appened array is: ", end="") #new array with 4 added
+for i in range (0, 4):
+    print (arr[i], end=" ")
+
+arr.insert(2, 5) #insert 5 at the second position in the array
+
+print("\r")
+
+print("New array is: ", end="")
+for i in range (0, 5):
+    print (arr[i], end=" ")
